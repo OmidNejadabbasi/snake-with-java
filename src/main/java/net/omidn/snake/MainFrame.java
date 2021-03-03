ca27689c0bf148299ed11a25b3d7cb5cffb25ede
Omid.N <nejadabbasio@gmail.com>
2021-03-04 02:02:53 +0330

    MainFrame will have spiritList so that it can draw the on its raster

--- src/main/java/net/omidn/snake/MainFrame.java
+++ src/main/java/net/omidn/snake/MainFrame.java
@@ -1,7 +1,10 @@
 package net.omidn.snake;
 
+import java.util.List;
+
 public class MainFrame {
     public int width, height;
-
+    public List<Spirit> spiritList;
+    private String[][] raster;
 
 }
