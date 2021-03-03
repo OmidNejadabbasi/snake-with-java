fe668dbf924e2d68c85cbfb50e023f38b63562f3
Omid.N <nejadabbasio@gmail.com>
2021-03-04 00:54:23 +0330

    Added a class that contains static methods fot basic terminalUtils

---
+++ src/main/java/net/omidn/snake/TerminalUtils.java
@@ -0,0 +1,60 @@
+package net.omidn.snake;
+
+import java.io.IOException;
+
+public class TerminalUtils {
+
+    /**
+     * It is equal to true if we are running on Microsoft Windows.
+     */
+    public static boolean isWindows = System.getProperty("os.name").toLowerCase().contains("windows");
+
+
+    /**
+     * Tries to clear the screen system independently. NOT test on Windows yet!
+     */
+    public static void clearScreen() {
+        if (isWindows) {
+            try {
+                Runtime.getRuntime().exec("cls");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            clearFromCellToEndOfScreen(0, 0);
+        }
+    }
+
+
+    /**
+     * Clears the screen form line number {@code line} and column number {@code column}.
+     */
+    public static void clearFromCellToEndOfScreen(int line, int column) {
+        System.out.printf("\033[%d;%dH\033[J", line, column);
+    }
+
+    /**
+     * Sets the cursor of terminal to blink-off mode.
+     */
+    public static void cursorBlinkOff() {
+        System.out.print("\033[?25l");
+    }
+
+
+    /**
+     * Reads a raw key input from the input but does not block the program. If there is no character present, it returns -2.
+     *
+     * @return
+     */
+    public static char readKey() {
+        char c = '\0';
+        try {
+            c = (char) RawConsoleInput.read(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return c;
+    }
+
+
+}
