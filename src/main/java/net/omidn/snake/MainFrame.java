ccc112ca46c5b558d0547a23d2ff8e501b8a6d12
Omid.N <nejadabbasio@gmail.com>
2021-03-04 02:14:20 +0330

    Added the resetRaster() method that will reset the raster and draw borders of the frame

--- src/main/java/net/omidn/snake/MainFrame.java
+++ src/main/java/net/omidn/snake/MainFrame.java
@@ -1,6 +1,7 @@
 package net.omidn.snake;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainFrame {
@@ -17,5 +18,25 @@ public class MainFrame {
         spiritList = new ArrayList<>();
         //drawing the border of frame
         raster = new String[height + 2][width + 2];
+
+        resetRaster();
+
+    }
+
+    private void resetRaster() {
+        //filling it with spaces.
+        for (String[] r : raster) {
+            Arrays.fill(r, " ");
+        }
+        raster[0][0] = "┌";
+        raster[0][width + 1] = "┐";
+        raster[height + 1][0] = "└";
+        raster[height + 1][width + 1] = "┘";
+        for (int i = 1; i < width + 1; i++) {
+            raster[0][i] = raster[height + 1][i] = "─";
+        }
+        for (int i = 1; i < height + 1; i++) {
+            raster[i][0] = raster[i][width + 1] = "│";
+        }
     }
 }
