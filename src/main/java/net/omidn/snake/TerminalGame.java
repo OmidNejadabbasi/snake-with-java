cd0635841d610c5e8f9704fa3853d3a67a120c86
Omid.N <nejadabbasio@gmail.com>
2021-03-04 00:35:10 +0330

    Initial setup

---
+++ src/main/java/net/omidn/snake/TerminalGame.java
@@ -0,0 +1,11 @@
+package net.omidn.snake;
+
+import com.sun.jna.Native;
+
+public class TerminalGame
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
