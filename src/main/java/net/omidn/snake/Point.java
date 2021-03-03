e83e919aa1b79f56a3319818fc272c4d9b2509a8
Omid.N <nejadabbasio@gmail.com>
2021-03-04 01:10:36 +0330

    Added a Point class the represents a char on the terminal

---
+++ src/main/java/net/omidn/snake/Point.java
@@ -0,0 +1,30 @@
+package net.omidn.snake;
+
+public class Point {
+
+    int x;
+    int y;
+    private char character;
+    private Color color;
+
+    public Point(int x, int y, char character) {
+        this(x, y, character, Color.WHITE);
+    }
+
+    public Point(int x, int y, char character, Color color) {
+        this.x = x;
+        this.y = y;
+        this.character = character;
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+
+}
