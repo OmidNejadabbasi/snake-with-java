2507511b22bfb9bf11134e11174bb9a2ce9d6cef
Omid.N <nejadabbasio@gmail.com>
2021-03-04 02:09:29 +0330

    Defined a public constructore for MainFrame

--- src/main/java/net/omidn/snake/MainFrame.java
+++ src/main/java/net/omidn/snake/MainFrame.java
@@ -1,5 +1,6 @@
 package net.omidn.snake;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainFrame {
@@ -7,4 +8,14 @@ public class MainFrame {
     public List<Spirit> spiritList;
     private String[][] raster;
 
+
+    public MainFrame(int width, int height) {
+        if (width < 1 || height < 1)
+            throw new IllegalArgumentException("Dimensions cannot be negative!");
+        this.width = width;
+        this.height = height;
+        spiritList = new ArrayList<>();
+        //drawing the border of frame
+        raster = new String[height + 2][width + 2];
+    }
 }
