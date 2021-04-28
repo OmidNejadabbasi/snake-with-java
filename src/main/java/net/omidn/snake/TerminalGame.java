94744f8c6af1b534a578a92061d49b236cc942ff
Omid.N <nejadabbasio@gmail.com>
2021-04-28 13:00:45 +0430

    creating the mainframe and adding a Snake to it

--- src/main/java/net/omidn/snake/TerminalGame.java
+++ src/main/java/net/omidn/snake/TerminalGame.java
@@ -10,5 +10,11 @@ public class TerminalGame
         System.out.println("Press WASD keys to move around.");
         System.out.println("Press P to pause and resume.");
 
+
+
+        MainFrame mainFrame = new MainFrame(60, 22);
+        Snake snake = new Snake(60, 22);
+        mainFrame.addSpirit(snake);
+
     }
 }
