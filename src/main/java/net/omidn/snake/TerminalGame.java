520d71a20b0c798806f34e67265aa1d209ca46cf
Omid.N <nejadabbasio@gmail.com>
2021-04-28 12:53:30 +0430

    getting the screen ready in TerminalGame

--- src/main/java/net/omidn/snake/TerminalGame.java
+++ src/main/java/net/omidn/snake/TerminalGame.java
@@ -1,11 +1,14 @@
 package net.omidn.snake;
 
-import com.sun.jna.Native;
-
 public class TerminalGame
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        TerminalUtils.clearScreen();
+        TerminalUtils.cursorBlinkOff();
+        System.out.println("Press WASD keys to move around.");
+        System.out.println("Press P to pause and resume.");
+
     }
 }
