0d9a1810ef72ecfcd9a87bb59f57646e3a9b1cd1
Omid.N <nejadabbasio@gmail.com>
2021-04-28 13:13:24 +0430

    Added the list of action handlers and action handling Thread

--- src/main/java/net/omidn/snake/TerminalGame.java
+++ src/main/java/net/omidn/snake/TerminalGame.java
@@ -1,9 +1,37 @@
 package net.omidn.snake;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class TerminalGame
 {
+    /**
+     * List of functions that accepts a code of pressed keys and does something
+     */
+    public static List<Consumer<Integer>> actionHandlers = new ArrayList<>();
+    /**
+     * Used for terminating the program
+     */
+    public static boolean running = true;
+
     public static void main( String[] args )
     {
+
+        new Thread(() -> {
+            try {
+                while (running) {
+                    Thread.sleep(80);
+                    int a = RawConsoleInput.read(false);
+                    actionHandlers.forEach(func -> func.accept(a));
+                }
+            } catch (InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
+
 
         TerminalUtils.clearScreen();
         TerminalUtils.cursorBlinkOff();
